3432a8c326ca2259c64d95ab0bfa91e93fe0a612
kosar <kosarhkhk@yahoo.com>
2020-11-06 13:27:26 +0330

    "debuging del_nod()"

--- app/src/dagger.py
+++ app/src/dagger.py
@@ -2,8 +2,8 @@ class Node:
     def __init__(self, label, val=None):
         self.__val = val
         self.__label = label
-        self.next = []
-        self.previous = []
+        self.next = None
+        self.previous = None
 
     def set_val(self, val):
         self.__val = val
@@ -19,15 +19,13 @@ class Node:
 
 
 class Dag:
-
     def __init__(self):
         self.nodes = []
-        # also set?
-        # self.head = None
+
     # @staticmethod
-    def find_path(self, visited: list, src_node, dest_node):
-        stack = []
-        stack.append(src_node)
+    def __findpath(self, visited: list, src_node, dest_node):
+        if src_node.next is None or dest_node.previous is None: return False
+        stack = [src_node]
         while len(stack):
             s = stack[-1]
             stack.pop()
@@ -44,68 +42,92 @@ class Dag:
         self.nodes.append(node)
         return node
 
+    def __delete_normal_node(self, node):
+        if node.next is not None :
+            temp_next=node.next.copy()
+            for adj in temp_next:
+                self.del_edge(node, adj)
+        return True
+
     def del_node(self, node, force_del=False):
-        if node in self.nodes:
-            if not force_del:
-                if len(node.previous) != 0:
-                    return "dependency error"
-                    # throw exception?
-            else:
-                if len(node.next) != 0:
-                    for adjacent in node.next:
-                        adjacent.previous.remove(node)
-                if len(node.previous) != 0:
-                    for adjacent in node.previous:
-                        adjacent.next.remove(node)
-                self.nodes.remove(node)
-        return "there is no such node"
+        def delete_previous(node,force_del):
+             if force_del:
+                temp_pr = node.previous.copy()
+                for pr_node in temp_pr:
+                    self.del_edge(pr_node, node)
+
+        def delete_next(node):
+            temp_next = node.next.copy()
+            for nxt_node in temp_next:
+                self.del_edge(node, nxt_node)
+
+
+        if node.previous is not None:
+            delete_previous(node,force_del)
+        if node.next is not None:
+            delete_next(node)
+        self.nodes.remove(node)
 
     def del_edge(self, src_node, dest_node):
-        if dest_node in src_node.next:
-            dest_node.previous.remove(src_node)
-            src_node.next.remove(dest_node)
+        if src_node.next is None or dest_node not in src_node.next:
+            return False
         else:
-            return "there is no such edge"
+            dest_node.previous.remove(src_node)
+            if len(dest_node.previous) == 0: dest_node.previous = None
+            src_node.next.remove(dest_node)
+            if len(src_node.next) == 0: src_node.next = None
+            return True
 
     def add_edge(self, src_node, dest_node):
+        if src_node.next is None: src_node.next = []
+        if dest_node.previous is None: dest_node.previous = []
         visited = []
-        if not self.find_path(visited, dest_node, src_node):
+        if not self.__findpath(visited, dest_node, src_node):
             src_node.next.append(dest_node)
             dest_node.previous.append(src_node)
-        print(f'visited ist : {[i.get_label() for i in visited]}')
 
-    def next(self):
+    def __next__(self):
+
         pass
 
     def show(self):
-        mydict={}
+        mydict = {}
+        mydict2 = {}
         for i in self.nodes:
-            if len(i.next) != 0 :
-                 mydict[i.get_label()] = [j.get_label() for j in i.next ]
+            if i.next is not None:
+                mydict[i.get_label()] = [j.get_label() for j in i.next]
             else:
                 mydict[i.get_label()] = []
-        print(mydict)
-        print([i.get_label() for i in self.nodes])
+        print(f' node -> next : {mydict}')
+
+        for i in self.nodes:
+            if i.previous is not None:
+                mydict2[i.get_label()] = [j.get_label() for j in i.previous]
+            else:
+                mydict2[i.get_label()] = []
+        print(f' node -> previous : {mydict2}')
 
 
+if __name__=='__main__':
+    a = Node("a", 2.3)
+    b = Node("b", 4.6)
+    c = Node("c", 4)
+    d = Node("d", 6)
+    e = Node("e", 6)
+    dag = Dag()
 
-f = Node("a", 2.3)
-s = Node("b", 4.6)
-t=Node("c", 4)
-fo=Node("d",6)
+    dag.add_node(a)
+    dag.add_node(b)
+    dag.add_node(c)
+    dag.add_node(d)
+    dag.add_node(e)
 
-d = Dag()
+    dag.add_edge(a, b)
 
-d.add_node(f)
-d.add_node(s)
-d.add_node(t)
-d.add_node(fo)
-
-d.add_edge(f, s)
-d.add_edge(s,t)
-d.add_edge(t,fo)
-d.add_edge(fo,f)
-
-
-d.show()
-
+    dag.add_edge(c, b)
+    dag.add_edge(e, a)
+    # dag.add_edge(c,a)
+    # dag.add_edge(e,a)
+    # dag.add_edge(d,c)
+    dag.del_node(a,True)
+    dag.show()
